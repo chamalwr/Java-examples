78defbf910a46c57a13d45919501600f585e5063
chamalwr <chamalkanishka4@gmail.com>
2019-02-20 23:19:39 +0530

    Hello World example with arg

---
+++ Hello_world_with_args/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from Java!");
+        System.out.println(args[0]);
+    }
+}
+
+
+// Compile Manullay - javac HelloWorld.java
+// Run Manullay - java HelloWorld firstElement
+/** output -
+ *   Hello World From Java
+ *   firstElement
+  */
