d8d5db5340c1621c4d2bf69f52b986e821584e96
chamalwr <chamalkanishka4@gmail.com>
2019-02-22 04:27:20 +0530

    saving objects with hibernate

--- hibernate-starter/src/main/java/com/chamalwr/func/CrudFunctions.java
+++ hibernate-starter/src/main/java/com/chamalwr/func/CrudFunctions.java
@@ -1,11 +1,36 @@
 package com.chamalwr.func;
 
 import com.chamalwr.model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class CrudFunctions {
 
-    public void saveStudent(Student student){
+    private SessionFactory sessionFactory;
 
+    public void saveStudent(Student... students){
+
+        try{
+             sessionFactory = new Configuration()
+                     .configure("hibernate.cfg.xml")
+                     .addAnnotatedClass(Student.class)
+                     .buildSessionFactory();
+
+
+            Session session = sessionFactory.getCurrentSession();
+
+            session.beginTransaction();
+            for (Student student : students){
+                session.save(student);
+                System.out.println("Student : " + student.getFirstName() + " saved successfully");
+            }
+            session.getTransaction().commit();
+        }catch (Exception err){
+           err.printStackTrace();
+        }finally {
+
+        }
     }
-    
+
 }
--- hibernate-starter/src/main/java/com/chamalwr/main/Main.java
+++ hibernate-starter/src/main/java/com/chamalwr/main/Main.java
@@ -1,10 +1,21 @@
 package com.chamalwr.main;
 
+import com.chamalwr.func.CrudFunctions;
 import com.chamalwr.model.Student;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        CrudFunctions crudFunctions = new CrudFunctions();
+
+        //Saving students (after saving one change emails - because emails are unique field in database)
+        Student student0 = new Student("Isuru", "Harinda", "isuru@email.aus");
+        Student student1 = new Student("Rimshan", "Mohomed", "rimshan@email.com");
+
+        crudFunctions.saveStudent(student0, student1);
+
+
 
     }
 
