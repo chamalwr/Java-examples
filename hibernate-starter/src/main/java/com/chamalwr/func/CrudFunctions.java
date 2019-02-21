26c40a72755836d6c258d1a66adba0897cbdad16
chamalwr <chamalkanishka4@gmail.com>
2019-02-22 03:22:25 +0530

    initial hibernate starter commit

---
+++ hibernate-starter/src/main/java/com/chamalwr/func/CrudFunctions.java
@@ -0,0 +1,11 @@
+package com.chamalwr.func;
+
+import com.chamalwr.model.Student;
+
+public class CrudFunctions {
+
+    public void saveStudent(Student student){
+
+    }
+    
+}
---
+++ hibernate-starter/src/main/java/com/chamalwr/main/Main.java
@@ -0,0 +1,11 @@
+package com.chamalwr.main;
+
+import com.chamalwr.model.Student;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
